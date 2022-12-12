7d63edccd303de4262a763e741a8903256845af7
abuoubaida <abuoubaida36@gmail.com>
2022-12-13 00:45:53 +0600

    register and login and dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RouteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,14 +19,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
+#.2. Group for Authenticate User Access+++++++++++++++++++++++++++++++++++++++++
+Route::get('admin',function (){
+    return redirect()->route('login');
+});
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    #.2.1.For Redirect Auth of role page++++++++++++++++++++++++++++++++++++++
+    Route::get('route-controller',[RouteController::class,'index'])->name('route.controller');
+    //--------------------End 2.1 Redirect Auth of role page-----------------
+    #.2.2.Group For admin role access++++++++++++++++++++++++++++++++++++++++
+    Route::group(['middleware' => ['auth','role:superadmin'],'prefix' => 'admin'],function(){
+        Route::controller(\App\Http\Controllers\AdminDashboardController::class)->group(function (){
+            Route::match(['get','post'],'dashboard','index')->name('admin.dashboard');
+
+        });
+    });
 });
 
 require __DIR__.'/auth.php';
